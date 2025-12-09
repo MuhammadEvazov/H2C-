7b53a755c2da0969a44d093ce79cf3ba66be2bae
MuhammadEvazov <muhammadevazov3@gmail.com>
2025-12-09 16:20:46 +0500

    classtask done 8/11

--- Clastask.cs
+++ Clastask.cs
@@ -1,9 +1,120 @@
-int Max(int val1, int val2)
-{
-    if (val1 > val2) return val1;
-    else return val2;
-}
-int val1, val2;
-val1 = Convert.ToInt32(Console.ReadLine());
-val2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Max(val1, val2));
+// Classtask
+
+// // Task1
+// void PrintFromNumberToOne(int n)
+// {
+//     for (int i = n; i > 0; i--)
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// PrintFromNumberToOne(n);
+
+// // Task2
+// int Sum(int a, int b, int c, int d)
+// {
+//     return a + b + c + d;
+// }
+// int a, b, c, d;
+// a = Convert.ToInt32(Console.ReadLine());
+// b = Convert.ToInt32(Console.ReadLine());
+// c = Convert.ToInt32(Console.ReadLine());
+// d = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Sum(a, b, c, d));
+
+// // Task3
+// int Reverse(int n)
+// {
+//     int rev = 0;
+//     for (int i = n; i > 0; i /= 10)
+//     {
+//         rev = rev * 10 + i % 10;
+//     }
+//     return rev;
+// }
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Reverse(n));
+
+// // Task4 Section1
+// void PrintStars(int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         Console.Write("*");
+//     }
+// }
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// PrintStars(n);
+
+// // Task4 section2
+// void PrintSquare(int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             Console.Write("*");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// PrintSquare(n);
+
+// // Task4 section3
+// void PrintRectangle(int n, int m){
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             Console.Write("*");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int n, m;
+// n = Convert.ToInt32(Console.ReadLine());
+// m = Convert.ToInt32(Console.ReadLine());
+// PrintRectangle(n, m);
+
+// // Task4 section4
+// void PrintTriangle(int s){
+//     for (int i = 1; i <= s; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             Console.Write("*");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int s;
+// s = Convert.ToInt32(Console.ReadLine());
+// PrintTriangle(s);
+
+// // Task5 section1
+// void PrintSpaces(int s)
+// {
+//     for (int i = 0; i < s; i++)
+//     {
+//         Console.Write(" ");
+//     }
+// }
+// void PrintStars(int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         Console.Write("*");
+//     }
+// }
+// int s, n;
+// s = Convert.ToInt32(Console.ReadLine());
+// n = Convert.ToInt32(Console.ReadLine());
+// PrintSpaces(n);
+// PrintStars(n);
+
+
